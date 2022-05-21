4d450fc0871b580cd31e5bed3b8af7177b6e2c82
nikita_litvinchuk <litvin4yk1303@icloud.com>
2022-05-21 15:47:02 +0200

    Корректировка решения заданий №1 и №2, решение заданий №3 и №4

--- Litvinchuk_Nikita_dz_3/task_3_1.py
+++ Litvinchuk_Nikita_dz_3/task_3_1.py
@@ -8,10 +8,7 @@
 def num_translate(english_number):
     num_translate_dict = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
                           'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
-    if english_number in num_translate_dict:
-        return num_translate_dict[english_number]
-    else:
-        return None
+    return num_translate_dict.get(english_number)
 
 
 # Задание №2. *(вместо задачи 1) Доработать предыдущую функцию в num_translate_adv():
@@ -20,13 +17,11 @@ def num_translate(english_number):
 # >>> num_translate_adv("One") "Один"
 # >>> num_translate_adv("two") "два"
 
+
 def num_translate_adv(english_number):
     num_translate_dict = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
                           'Zero': 'Ноль', 'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре', 'Five': 'Пять',
                           'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять',
                           'Six': 'Шесть', 'Seven': 'Семь', 'Eight': 'Восемь', 'Nine': 'Девять', 'Ten': 'Десять'}
-    if english_number in num_translate_dict:
-        return num_translate_dict[english_number]
-    else:
-        return None
+    return num_translate_dict.get(english_number)
 
